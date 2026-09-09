90a9c84a4f6b848fc116efc028f6dd1bd6daeb57
SamTSham <madwar@mac.com>
2026-09-09 12:13:45 +0200

    Add files via upload

--- src/Turntable.rb
+++ src/Turntable.rb
@@ -8,7 +8,7 @@ module SamTurntable
   unless file_loaded?(__FILE__)
     ex = SketchupExtension.new(EXTENSION_NAME, 'Turntable/main')
     ex.description = 'Compact theatrical revolve control with named pegs, timed transitions, snapping, long-way movement, and adjustable acceleration/deceleration. Settings are stored inside each SketchUp model.'
-    ex.version     = '1.0.0'
+    ex.version     = '1.0.1'
     ex.creator     = 'Sam Madwar'
     ex.copyright   = 'Copyright 2026 Sam Madwar'
     Sketchup.register_extension(ex, true)
